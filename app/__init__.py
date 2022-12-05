f166292da72b7ea7a049f76ac3e80c1363af8981
samsonahh <swu30@stuy.edu>
2022-12-05 14:04:41 -0500

    Init a bunch of stuff

---
+++ app/__init__.py
@@ -0,0 +1,19 @@
+# TNPG: It's Friday!, Roster: Erica Li, Verit Li, Daniel He, Samson Wu
+# SoftDev
+# P01
+
+from flask import Flask, render_template, request, session, redirect, url_for
+import requests
+from db_functions import *
+
+app = Flask(__name__)
+app.secret_key = ''
+
+@app.route('/')
+def index():
+    return 'hi'
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
---
+++ app/db_functions.py
@@ -0,0 +1,3 @@
+# TNPG: It's Friday!, Roster: Erica Li, Verit Li, Daniel He, Samson Wu
+# SoftDev
+# P01
