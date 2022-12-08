dfa99dc6245ec959b209311d97ebacbde6da5077
samsonahh <swu30@stuy.edu>
2022-12-08 10:59:21 -0500

    Navbar formatting on all pages and setup flask routes

--- app/__init__.py
+++ app/__init__.py
@@ -5,13 +5,43 @@
 from flask import Flask, render_template, request, session, redirect, url_for
 import requests
 from db_functions import *
+from api_functions import *
 
 app = Flask(__name__)
-app.secret_key = ''
+app.secret_key = b'kJu2hlllSnasd8a0a@(@2lask'
 
-@app.route('/')
-def index():
-    return 'hi'
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    loginstatus = False
+    return render_template("home.html", login_status = loginstatus)
+
+@app.route('/login')
+def login():
+    return render_template("login.html")
+
+@app.route('/register')
+def register():
+    return render_template("register.html")
+
+@app.route('/logout')
+def logout():
+    return render_template("login.html")
+
+@app.route('/profile')
+def profile():
+    loginstatus = False
+    return render_template("profile.html", login_status = loginstatus)
+
+@app.route('/match')
+def match():
+    loginstatus = False
+    return render_template("match.html", login_status = loginstatus)
+
+@app.route('/compatibility')
+def compatibility():
+    loginstatus = False
+    return render_template("compatibility.html", login_status = loginstatus)
+
 
 if __name__ == "__main__": #false if this file imported as module
     #enable debugging, auto-restarting of server when this file is modified
