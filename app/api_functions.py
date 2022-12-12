073e1a2f6baec09f769a9f2465ea93f265305233
Daniel He <dhe30@stuy.edu>
2022-12-12 14:05:46 -0500

    api

--- app/api_functions.py
+++ app/api_functions.py
@@ -33,17 +33,27 @@ def get_random_quote(character):
 
 #uses AnimeChan API to get a random character and their quotes and their information from Kitsu API
 def get_random_profile():
+    #First get a random character with eligible quotes
     AnimeChan_url = 'https://animechan.vercel.app/api/random'
     res = requests.get(AnimeChan_url) 
     json = res.json() 
     quote = json['quote']
-    character = json['character']
+    character = json['character'] #Use accurate name returned by animechan to search in Kitsu API
     url =  "https://kitsu.io/api/edge/characters?"
     res = requests.get(url, params={"filter[name]": character})
-    return res.json()
-#print(get_random_profile())
+    #Only looks at the first character returned
+    json = res.json()
+    en_name = json["data"][0]["attributes"]["names"]["en"]
+    jp_name = json["data"][0]["attributes"]["names"]["ja_jp"]
+    description = json["data"][0]["attributes"]["description"]
+    image = json["data"][0]["attributes"]["image"]["original"]
+    #find ten quotes from animechan 
+    AnimeChan_url_ten = "https://animechan.vercel.app/api/quotes/character?name=" + character
+    res = requests.get(AnimeChan_url_ten) 
+    json = res.json()
+    quotes = []
+    for a in json:
+        quotes.append(a['quote'])
+    return json
 
-
-
-
-    
+print(get_random_profile())
