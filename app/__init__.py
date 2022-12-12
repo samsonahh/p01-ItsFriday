b63c2e6f82551cd0f526ddce50463350611d93f9
samsonahh <swu30@stuy.edu>
2022-12-12 14:49:40 -0500

    bug fixes by samson and devlog for 12/12

--- app/__init__.py
+++ app/__init__.py
@@ -6,84 +6,106 @@ from flask import Flask, render_template, request, session, redirect, url_for
 import sqlite3
 import requests
 from db_functions import *
-from api_functions import *
+#from api_functions import *
 from register_functions import * 
 ########## NOTES ###########
 
-# Tables for database need to be created first before login/ register works 
+# Samson: fixed a lot of bugs
 
 ########## NOTES ###########
+
+generate_preset_database()
+
 app = Flask(__name__)
 app.secret_key = b'kJu2hlllSnasd8a0a@(@2lask'
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
-    loginstatus = False #Samson: We should check session whether user is logged in
-    return render_template("home.html", login_status = loginstatus)
+    loginstatus = False
+    sessionusername = ''
+    if 'username' in session:
+        loginstatus = True
+        sessionusername = session['username']
+    return render_template("home.html", login_status = loginstatus, session_username = sessionusername)
 
-@app.route('/login', methods=['GET', 'POST']) #, methods=['POST'])
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if 'username' in session: #if someone tries to login while already logged in
+        return redirect(url_for('home'))
+
     if request.method == 'POST':
-        db = sqlite3.connect(DB_FILE, check_same_thread=False) #opens if file exists... if not, it will create one
-        c = db.cursor() #be able to execute & operate 
         username = request.form['username']
         password = request.form['password']
         if check_userexists(username) and get_user_password(username) == password: #checks if user exists & password matches
-            db.close()
             session['username'] = request.form['username'] #logs in user
-            loginstatus = True 
             return redirect(url_for('home'))
-        if not check_userexists(username, c): #checks if username exists 
-            db.close()
-            return render_template('login.html', FAILMSG ="Username does not exist")
+        if not check_userexists(username): #checks if username exists 
+            return render_template('login.html', FAILMSG ="Username does not exist!")
         else: 
-            db.close()
-            return render_template('login.html', FAILMSG ="Username and password does not match")
+            return render_template('login.html', FAILMSG ="Username and password does not match!")
     return render_template("login.html", FAILMSG = "")
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if 'username' in session: #if someone tries to register while already logged in
+        return redirect(url_for('home'))
+
     if request.method == 'POST':
-        print("checkpoint!")
-        db = sqlite3.connect(DB_FILE, check_same_thread=False) #opens if file exists... if not, it will create one
-        c = db.cursor() #be able to execute & operate 
-        new_username = request.form['username']
-        new_password = request.form['password']
-    
+        new_username = request.form['register_username'].strip() #strip removes whitespace. prevents accounts with blank usernames and passwords
+        new_password = request.form['register_password'].strip()
+        new_password_confirm = request.form['register_password_confirm'].strip()
+
+        print(new_username, new_password, new_password_confirm)
+
         if check_userexists(new_username): #checks if the username is already taken
-            return render_template("register.html", FAILMSG="Username already exists")
-        
-        if check_usernamerequirements(new_username): #checks if the username fulfills reqs 
+            return render_template("register.html", FAILMSG="Username already exists!")
+
+        if not check_usernamerequirements(new_username): #checks if the username fulfills reqs 
             return render_template("register.html", FAILMSG="Username is not long enough")
        
-        if check_passwordrequirements(new_password): #checks if the password fulfills reqs
+        if not check_passwordrequirements(new_password): #checks if the password fulfills reqs
             return render_template("register.html", FAILMSG="Password is not long enough")
+
+        if not new_password == new_password_confirm:
+            return render_template("register.html", FAILMSG="Passwords don't match!")
        
-        else:
-            add_newuser(new_username, new_password) #adds user to the database
-            session['username'] = request.form['username'] #logs in user
-            loginstatus = True 
-            return redirect(url_for('home'))
+        add_newuser(new_username, new_password) #adds user to the database
+        session['username'] = new_username #logs in user
+        return redirect(url_for('home'))
     return render_template("register.html", FAILMSG="")
 
 @app.route('/logout')
 def logout():
-    return render_template("login.html")
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('home'))
 
 @app.route('/profile')
 def profile():
     loginstatus = False
-    return render_template("profile.html", login_status = loginstatus)
+    sessionusername = ''
+    if 'username' in session:
+        loginstatus = True
+        sessionusername = session['username']
+    return render_template("profile.html", login_status = loginstatus, session_username = sessionusername)
 
 @app.route('/match')
 def match():
     loginstatus = False
-    return render_template("match.html", login_status = loginstatus)
+    sessionusername = ''
+    if 'username' in session:
+        loginstatus = True
+        sessionusername = session['username']
+    return render_template("match.html", login_status = loginstatus, session_username = sessionusername)
 
 @app.route('/compatibility')
 def compatibility():
     loginstatus = False
-    return render_template("compatibility.html", login_status = loginstatus)
+    sessionusername = ''
+    if 'username' in session:
+        loginstatus = True
+        sessionusername = session['username']
+    return render_template("compatibility.html", login_status = loginstatus, session_username = sessionusername)
 
 
 if __name__ == "__main__": #false if this file imported as module
--- app/db_functions.py
+++ app/db_functions.py
@@ -1,12 +1,11 @@
 # TNPG: It's Friday!, Roster: Erica Li, Verit Li, Daniel He, Samson Wu
 # SoftDev
 # P01
+# fetchall attempts to get everything while fetchone gets first line
 
 import sqlite3
 from datetime import datetime
 DB_FILE="ALC.db" #ALC = Anime Love Calculator 
-
-## Samson: I recommend opening and closing/committing the database in every single function to make it easier in Flask.
 
 def reset_database():
     db = sqlite3.connect(DB_FILE) #open if file exists, if not it will create a new db
@@ -29,33 +28,39 @@ def add_newuser(username, password):
     c = db.cursor() #creates db cursor to execute and fetch           
 
     c.execute("INSERT INTO users VALUES(?,?)", data)
-    db.commit() 
+    db.commit()
+    db.close()
 
 #checks whether the user already exists in the database
 def check_userexists(username):
-    db = sqlite3.connect(DB_FILE) #open if file exists, if not it will create a new db      
+    db = sqlite3.connect(DB_FILE) #open if file exists, if not it will create a new db 
+    c = db.cursor() #creates db cursor to execute and fetch      
 
     c.execute("SELECT * FROM users WHERE username=?", (username,))
-    dic = c.fetchone()
+    dict = c.fetchone()
 
-    return dic[1] #returns a password for the given username
-    else
-        return ("User does not exist")
+    db.close()
 
-    db.commit()
+    if dict == None: #if no user
+        return False
+    
+    return True #if dict is not empty (meaning user exists)
 
 #gets the user's password from the database
 def get_user_password(username):
     db = sqlite3.connect(DB_FILE) #open if file exists, if not it will create a new db          
+    c = db.cursor() #creates db cursor to execute and fetch      
 
-    c.execute("SELECT * FROM users WHERE password=?", (password,))
-    dic = c.fetchall()
+    c.execute("SELECT * FROM users WHERE username=?", (username,))
+    dict = c.fetchone()
 
-    if dic == []:
-        return False
-    else:
-        return True
+    db.close()
 
-    db.commit()
+    return dict[1]
 
-
+def generate_preset_database():
+    reset_database()
+    add_newuser('samson', 'samson123')
+    add_newuser('erica', 'erica123')
+    add_newuser('verit', 'verit123')
+    add_newuser('daniel', 'daniel123')
