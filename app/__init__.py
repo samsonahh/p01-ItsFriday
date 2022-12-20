da9de61ee5d27d685f8ddb13f61ff0e24369ede5
Daniel He <dhe30@stuy.edu>
2022-12-20 13:56:46 -0500

    yes

--- app/__init__.py
+++ app/__init__.py
@@ -240,8 +240,8 @@ def match_test():
 def character_profile(id):
     if not 'username' in session: #if someone tries to go here when not logged in
         return redirect(url_for('login'))
-
-    return render_template("profile.html", session_username = session['username'], display_id = id)
+    dictionary = api.get_char_info_by_id(id)
+    return render_template("profile.html", session_username = session['username'], dict = dictionary)
 
 
 @app.route('/compatibility', methods=['GET', 'POST'])
